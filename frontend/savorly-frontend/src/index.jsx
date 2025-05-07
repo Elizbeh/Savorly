f624e1ec7b4a383e6d7e4dcc64eed295ee7f09e5
Elizbeh <eliz01_01@yahoo.com>
2025-05-07 09:07:55 +0200

    Ready to deploy to Github Pages

--- frontend/savorly-frontend/src/index.jsx
+++ frontend/savorly-frontend/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext'; // adjust this path to your actual file
+import { AuthProvider } from './contexts/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
